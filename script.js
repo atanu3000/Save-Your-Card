fb4a8cca402099d959716406d0c1b5a1fdfec34a
Atanu Paul <atanupaul2411@gmail.com>
2023-09-24 23:54:28 +0530

    filtering cards

--- script.js
+++ script.js
@@ -37,3 +37,54 @@ const fetchData = () => {
 }
 
 fetchData();
+
+const fetchUserData = async () => {
+    try {
+        const response = await fetch('data.json');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching JSON data:', error);
+    }
+}
+
+const searchUsers = async () => {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const userData = await fetchUserData();
+    cards.innerHTML = '';
+
+    const filteredData = userData.filter(user => {
+        const name = user.name.toLowerCase();
+        const email = user.email.toLowerCase();
+        return name.includes(searchInput) || email.includes(searchInput);
+    });
+
+    // Display search results
+    filteredData.forEach(item => {
+        const searchCard = document.createElement('div');
+                searchCard.id = 'card';
+                searchCard.className = 'mx-auto w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-6';
+
+                searchCard.innerHTML = `
+                    <div class="flex flex-col items-center py-6">
+                        <img
+                            class="w-24 h-24 mb-3 rounded-full shadow-lg"
+                            src="${item.image}"
+                            alt="${item.name} image"
+                        />
+                        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                            ${item.name}
+                        </h5>
+                        <span class="text-sm text-gray-500 dark:text-gray-400">
+                            ${item.email}
+                        </span>
+                        <div class="flex mt-4 space-x-3 md:mt-6">
+                            <p class="px-5 text-center text-black">${item.description}</p>
+                        </div>
+                    </div>
+                `;
+        cards.appendChild(searchCard);
+    });
+}
+
+document.getElementById('searchInput').addEventListener('input', searchUsers);
